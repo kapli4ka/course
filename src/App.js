20f499e39282d377b72ec323f3bfe69fc1bef921
Oleh Bartian <neveseloeshow@gmail.com>
2022-12-04 14:29:37 +0200

    Добавлен localStorage для хранение статуса авторизации при обновлении

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ import {AuthContext} from "./context";
 
 function App() {
     const [isAuth, setIsAuth] = useState(false)
+
+    useEffect(()=> {
+        if(localStorage.getItem('auth')){
+            setIsAuth(true)
+        }
+    },[])
     return (
         <AuthContext.Provider value={
             {
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,9 +5,14 @@ import {AuthContext} from "../context";
 
 const Login = () => {
     const { setIsAuth} = useContext(AuthContext)
+    const login = () =>{
+        setIsAuth(true)
+        localStorage.setItem('auth', 'true')
+    }
+    console.log(localStorage)
     return (
         <div>
-            <form onSubmit={e => setIsAuth(true)}>
+            <form onSubmit={login}>
                 <h1>Логин</h1>
                 <MyInput type={'text'} placeholder={'Логин'} />
                 <MyInput type={'password'} placeholder={'Пароль'} />
